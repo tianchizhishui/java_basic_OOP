f472028fded724f76baab35dbbb4b64b0dd18ffe
Tianchi (David) Liu <55475915+tianchizhishui@users.noreply.github.com>
2019-10-15 19:37:22 -0700

    explanation and example of inheritance

---
+++ Inheritance/src/com/company/Dog.java
@@ -0,0 +1,50 @@
+package com.company;
+
+public class Dog extends Animal {
+
+    private int eyes;
+    private int legs;
+    private int tails;
+    private int teeth;
+    private String coat;
+
+    public Dog(String name, int size, int weight, int eyes, int legs, int tails, int teeth, String coat) {
+        super(name, 1, 1, size, weight);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tails = tails;
+        this.teeth = teeth;
+        this.coat = coat;
+    }
+
+    private void chew() {
+        System.out.println("Dog.chew() called");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Dog.eat() called");
+        chew();
+        super.eat();
+    }
+
+    public void walk() {
+        System.out.println("Dog.walk() called");
+        super.move(5);
+    }
+
+    private void moveLegs(int speed) {
+        System.out.println("Dog moveLegs() called");
+    }
+    @Override
+    public void move(int speed) {
+        System.out.println("Dog.move() called");
+        moveLegs(speed);
+        super.move(speed);
+    }
+
+    public void run() {
+        System.out.println("Dog.run() called");
+        move(10);
+    }
+}
